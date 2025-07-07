891f291e8c34745a55d729df7a5215e4a2d03534
unknown <kaborges0203@gmail.com>
2025-07-07 00:30:56 -0300

    Adicionando arquivos do projeto completo

---
+++ Doacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoacaoRacao
+{
+    //  Doação
+    public class Doacao
+    {
+        public string NomeDoador { get; set; }
+        public double QuantidadeKg { get; set; }
+        public string Local { get; set; }
+
+        public void RegistrarDoacao()
+        {
+            Console.WriteLine("\nDoação registrada com sucesso!");
+            Console.WriteLine($"Obrigado, {NomeDoador}!");
+            Console.WriteLine($"Você doou {QuantidadeKg} kg de ração.");
+            Console.WriteLine($"O alimento será destinado ao local: {Local}");
+            Console.WriteLine("Os cães agradecem sua ajuda para continuarem alimentados!\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DoacaoRacao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Sistema de Doação de Ração para Cachorros ===");
+
+        // Criando objeto da classe Doacao
+        Doacao novaDoacao = new Doacao();
+
+        // Coletando dados do usuário
+        Console.Write("Digite seu nome: ");
+        novaDoacao.NomeDoador = Console.ReadLine();
+
+        Console.Write("Digite a quantidade de ração em kg: ");
+        double quantidade;
+        while (!double.TryParse(Console.ReadLine(), out quantidade) || quantidade <= 0)
+        {
+            Console.Write("Quantidade inválida. Digite novamente: ");
+        }
+        novaDoacao.QuantidadeKg = quantidade;
+
+        Console.Write("Digite o local de doação (bairro ou ONG): ");
+        novaDoacao.Local = Console.ReadLine();
+
+        // Mostrando resultado
+        novaDoacao.RegistrarDoacao();
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
